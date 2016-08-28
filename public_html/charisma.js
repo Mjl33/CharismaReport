9050569dd79469a37c75b6f7a53a705ef857a7e5
MJ <mjlee.1@gmail.com>
2016-08-29 01:27:32 +0800

    Bars are now generated

--- public_html/charisma.js
+++ public_html/charisma.js
@@ -1,159 +1,85 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//var width = 420,
-//        barHeight = 20;
-//
-//var x = d3.scaleLinear()
-//        .range([0, width]);
-//
-//var chart = d3.select(".chart")
-//        .attr("width", width);
-//
-//var data = d3.tsv("./data/data.tsv", type, function (error, data) {
-//    x.domain([0, d3.max(data, function (d) {
-//            return d.value;
-//        })]);
-//
-//    chart.attr("height", barHeight * data.length);
-//
-//    var bar = chart.selectAll("g")
-//            .data(data)
-//            .enter().append("g")
-//            .attr("transform", function (d, i) {
-//                return "translate(0," + i * barHeight + ")";
-//            });
-//
-//    bar.append("rect")
-//            .attr("width", function (d) {
-//                return x(d.value);
-//            })
-//            .attr("height", barHeight - 1);
-//
-//    bar.append("text")
-//            .attr("x", function (d) {
-//                return x(d.value) - 3;
-//            })
-//            .attr("y", barHeight / 2)
-//            .attr("dy", ".35em")
-//            .text(function (d) {
-//                return d.value;
-//            });
-//});
-//
-//function type(d) {
-//    d.value = +d.value; // coerce to number
-//    return d;
-//}
+var x = d3.scaleBand()
+    .rangeRound([0, width])
+    .padding(0.1)
+    .align(0.1);
 
-var m = [20, 20, 30, 20],
-        w = 960 - m[1] - m[3],
-        h = 500 - m[0] - m[2];
+var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
 
-var x,
-        y,
-        duration = 1500,
-        delay = 500;
+var z = d3.scaleOrdinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-var svg = d3.select("body").append("svg")
-        .attr("width", w + m[1] + m[3])
-        .attr("height", h + m[0] + m[2])
-        .append("g")
-        .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+var stack = d3.stack();
 
-var dates,
-        epics;
+d3.csv("./data/data.csv", type, function (error, data) {
+    if (error) throw error;
 
-d3.csv("./data/data.csv", function (data) {
-    var parseTime = d3.timeParse("%d/%e/%Y");
+    // x.domain(mindate, maxdate)
+    x.domain([d3.min(data, function(d){return d.date;}), d3.max(data, function(d){return d.date;})]);
+    //y.domain(0, maxSumTcCount)
+    y.domain([0, d3.max(data, function (d) {
+        return d.total;
+    })]).nice();
+    //map z ranges to distinct epics
+    z.domain(data.columns.slice(1));
 
-    //nest data values by date
-    byDates = d3.nest()
-            .key(function (d) {
-                return d.date;
-            })
-            .sortKeys(d3.ascending)
-            .entries(data);
+    //construct stacked bar
+    g.selectAll(".serie")
+        .data(stack.keys(data.columns.slice(1))(data))
+        .enter().append("g")
+        .attr("class", "serie")
+        .attr("fill", function(d) { return z(d.key); })
+        .selectAll("rect")
+        .data(function(d) { return d; })
+        .enter().append("rect")
+        .attr("x", function(d) { return x(d.data.date); })
+        .attr("y", function(d) { return y(d[1]); })
+        .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+        .attr("width", 15);
 
-    //parse dates and numbers
-    //compute maximum numbers per date, needed for y-domain
-    byDates.forEach(function (s) {
-        s.values.forEach(function (d) {
-            d.date = parseTime(d.date);
-            d.tcCount = +d.tcCount;
-        });
-        s.maxValue = d3.max(s.values, function (d) {
-            return d.value;
-        });
-        s.sumValue = d3.sum(s.values, function (d) {
-            return d.value;
-        });
-    });
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
-    //insert by dates
-    var g = svg.selectAll("g")
-            .data(dates)
-            .enter().append("g")
-            .attr("class", "date");
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).ticks(10, "s"))
+        .append("text")
+        .attr("x", 2)
+        .attr("y", y(y.ticks(10).pop()))
+        .attr("dy", "0.35em")
+        .attr("text-anchor", "start")
+        .attr("fill", "#000")
+        .text("Test Cases");
 
-    //generate the stackedBar
-//    setTimeout(stackedBar, duration);
-    stackedBar();
+    var legend = g.selectAll(".legend")
+        .data(data.columns.slice(1).reverse())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; })
+        .style("font", "10px sans-serif");
+
+    legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .attr("fill", z);
+
 });
 
-function stackedBar() {
-    x = d3.scaleBand()
-            .range([0, w - 60])
-            .round(.1);
-
-    var stack = d3.stack()
-            .keys(function (d) {
-                return d.values;
-            })
-            .order(d3.stackOrderNone)
-            .offset(d3.stackOffsetNone);
-
-    var g = svg.selectAll(".date");
-    stack(dates);
-
-    y.domain([0, d3.max(dates[0].values.map(function (d) {
-            return d.value + d.value0;
-        }))])
-            .range([h, 0]);
-    var t = g.transition()
-            .duration(duration / 2);
-
-    t.select("text")
-            .delay(dates[0].values.length * 10)
-            .attr("transform", function (d) {
-                d = d.values[d.values.length - 1];
-                return "translate(" + (w - 60) + "," + y(d.value / 2 + d.value0) + ")";
-            });
-
-    t.selectAll("rect")
-            .delay(function (d, i) {
-                return i * 10;
-            })
-            .attr("y", function (d) {
-                return y(d.value0 + d.value);
-            })
-            .attr("height", function (d) {
-                return h - y(d.value);
-            })
-            .each("end", function () {
-                d3.select(this)
-                        .style("stroke", "#fff")
-                        .style("stroke-opacity", 1e-6)
-                        .transition()
-                        .duration(duration / 2)
-                        .attr("x", function (d) {
-                            return x(d.date);
-                        })
-                        .attr("width", x.rangeBand())
-                        .style("stroke-opacity", 1);
-            });
-
+function type(d, i, columns) {
+    parseDate = d3.timeParse("%d/%m/%Y");
+    d.date = parseDate(d.date);
+    for(i=1, t=0; i< columns.length; ++i)
+        t += d[columns[i]] = +d[columns[i]];
+    d.total=t;
+    return d;
 }
+
